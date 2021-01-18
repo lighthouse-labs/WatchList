ba114420ba79bbdc6c650d8dfdf6e143fba35a36
MikeTheFyke <mikefyke@hotmail.com>
2021-01-17 19:40:19 -0500

    Added on mouse leave event to submit button

--- src/WatchInput.js
+++ src/WatchInput.js
@@ -10,10 +10,14 @@ class WatchInput extends Component {
             TweenMax.to('#watch-search', 1, { backgroundColor: '#282c34', color: '#8fd1eb', borderColor: '#8fd1eb'})
         }
 
+        const onMouseLeave = (event) =>{
+            TweenMax.to('#watch-search', 1, { backgroundColor: '#8fd1eb', color: '#282c34', borderColor: 'transparent'})
+        }
+
         return(
             <div id="watch-input-container">
                 <input id="watch-input" placeholder="What would you like to watch ?"></input>
-                <button id="watch-search" onMouseOver={onMouseOver}>Search</button>
+                <button id="watch-search" onMouseOver={onMouseOver} onMouseLeave={onMouseLeave}>Search</button>
             </div>
         );
     }
