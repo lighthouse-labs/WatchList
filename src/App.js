8c9b06268444410f821edcca3c371ae9f46de1aa
MikeTheFyke <mikefyke@hotmail.com>
2021-01-18 21:26:03 -0500

    Set initial state moved from constructor to componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,53 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
 import WatchInput from './WatchInput';
-// import MovieOutput from './MovieOutput';
-
 
 class App extends Component {
 
   constructor(){
     super();
     this.state = {
-      media: [
-        {
+      media: []
+    }
+  }
+
+  componentDidMount(){
+    this.setState({media: [
+              {
           title: "Jupiter's Legacy",
           category: "TV Show",
-          release: "March 2021"
+          release: "March 00 2021"
         },
         {
           title: "The King's Man",
           category: "Movie",
           release: "March 12 2021"
         }
-      ]
-    }
+    ]});
   }
 
    render() {
 
-    // addMovie = (newMovie) => {
-    //   console.log(this.props.movies);
-    //   // let movie = this.props.movies;
-    // }
-    
-
     return (
       <div className="App">
         <header className="App-header">
-          {/* <MovieOutput media={this.state.media}/> */}
           <WatchInput media={this.state.media}/>
-          {/* <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p> */}
-          <a
-            className="App-link"
-            href="https://wikipedia.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Wikipedia
-          </a>
         </header>
       </div>
     );
--- src/MovieOutput.js
+++ src/MovieOutput.js
@@ -5,18 +5,10 @@ import './MovieOutput.css';
 class MovieOutput extends Component {
     render(){
         
-        // console.log(this.props.media[0].title)
-        // addMovie = (newMovie) => {
-        //     console.log(this.props.movies);
-        //     // let movie = this.props.movies;
-        // }
-
         return(
-            // <div id="movie-output-container">
                 <li className="media-info">
                     <strong>{this.props.media.title}</strong>:  {this.props.media.release} {this.props.media.category}  
                 </li>
-            // </div>
         );
     }
 }
