3d4a08ea494f4f07fae85f01e34a32395dcd9f10
MikeTheFyke <mikefyke@hotmail.com>
2021-01-19 17:15:13 -0500

    Created the deleteMedia method

--- src/MovieOutput.js
+++ src/MovieOutput.js
@@ -3,13 +3,16 @@ import './MovieOutput.css';
 
 
 class MovieOutput extends Component {
+    
+    deleteMedia(){
+        this.props.onDelete(this.props.media.id);
+    }
+
+
     render(){
-        
-
-
         return(
                 <li className="media-info">
-                    <strong>{this.props.media.title}</strong>:  {this.props.media.release} {this.props.media.category} <a href="#" onClick={this.deleteMedia.bind(this)}>X</a> 
+                    <strong>{this.props.media.title}</strong>:  {this.props.media.release} {this.props.media.category} <button onClick={this.deleteMedia.bind(this, this.props.media.id)}>X</button> 
                 </li>
         );
     }
