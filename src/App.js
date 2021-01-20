d895c5ae0b7adc6cb3b8754bcb31c7d729e1f9d6
MikeTheFyke <mikefyke@hotmail.com>
2021-01-19 20:40:57 -0500

    using cheerio to grab poster source title and release year

--- src/App.js
+++ src/App.js
@@ -1,44 +1,52 @@
 import React, { Component } from 'react';
-import {v4 as uuidv4} from "uuid"; 
+// import {v4 as uuidv4} from "uuid"; 
 import cheerio from 'cheerio';
 import './App.css';
 import WatchInput from './WatchInput';
+import { _checkPlugin } from 'gsap/gsap-core';
 
 class App extends Component {
 
   constructor(){
     super();
     this.state = {
-      media: []
+      media: [],
+      data: [],
+      newReleases: []
     }
   }
 
-  getMedia(){
-    fetch('https://theskylive.com/mars-tracker', (error, response, html) => {
-      if (!error && response.statusCode === 200){
-        const $ = cheerio.load(html)
-    
-        const earthMars = $('#disearth');
-        const earthMarsDistance = earthMars.text();
-        console.log("Grabbed : " + earthMarsDistance);
-      }
+  getMedia =() => {
+    fetch('https://www2.bflix.to/')
+    .then(response => response.text())
+    .then(data => {
+      const $ = cheerio.load(data);
+      const mediaTitle = $ ('.poster').attr('title');
+      const mediaRelease =$ )_checkPlugin;
+      const posterSource = $('.poster img').attr('src');
+      console.log(mediaTitle);
+      console.log(posterSource);
+      this.setState({newReleases: $(".posterSource").html() });
+    })
+    .catch(error => {
+      this.setState({error: error});
     });
-  }
+  };
 
   componentDidMount(){
     this.setState({media: [
-        {
-          id: uuidv4(),
-          title: "Jupiter's Legacy",
-          category: "TV Show",
-          release: "March 00 2021"
-        },
-        {
-          id: uuidv4(),
-          title: "The King's Man",
-          category: "Movie",
-          release: "March 12 2021"
-        }
+        // {
+        //   id: uuidv4(),
+        //   title: "Jupiter's Legacy",
+        //   category: "TV Show",
+        //   release: "March 00 2021"
+        // },
+        // {
+        //   id: uuidv4(),
+        //   title: "The King's Man",
+        //   category: "Movie",
+        //   release: "March 12 2021"
+        // }
     ]});
     this.getMedia();
   }
