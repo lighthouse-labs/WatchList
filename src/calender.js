c2ee3c3f2dfa1dbaa5ac900565f59082469865b6
MikeTheFyke <mikefyke@hotmail.com>
2021-01-21 20:54:17 -0500

    Added calender react scaffolding

---
+++ src/calender.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+class Calender extends Component {
+
+    constructor(){
+        super();
+        this.state = {
+        }
+    }
+
+
+    render(){
+
+        return(
+            <div id="calender-container">
+
+            </div>
+        );
+    }
+}
+
+
+export default Calender;
